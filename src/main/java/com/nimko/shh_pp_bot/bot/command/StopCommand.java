7a2b75a35cee01e0d4dd0e99f1b410f34b7a3cbe
sn <shurick2211@gmail.com>
2022-12-09 10:31:03 +0200

    logging.level++

--- src/main/java/com/nimko/shh_pp_bot/bot/command/StopCommand.java
+++ src/main/java/com/nimko/shh_pp_bot/bot/command/StopCommand.java
@@ -2,8 +2,9 @@ package com.nimko.shh_pp_bot.bot.command;
 
 import com.nimko.shh_pp_bot.bot.command_service.Command;
 import com.nimko.shh_pp_bot.bot.send_service.SendMessService;
+import lombok.extern.slf4j.Slf4j;
 import org.telegram.telegrambots.meta.api.objects.Message;
-
+@Slf4j
 public class StopCommand implements Command {
     private final SendMessService sendMessService;
 
@@ -15,6 +16,7 @@ public class StopCommand implements Command {
     public void execute(Message message) {
         String mess = "Зупинка Бота!";
         sendMessService.send(message.getChatId().toString(), mess);
+        log.warn("Bot was stopped!");
         System.exit(1);
     }
 }
